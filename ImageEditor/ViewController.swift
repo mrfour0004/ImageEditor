b9ea354a5c1b61f4536efb6ca15f97e1407a5dfb
mrfour <mrfour0004@outlook.com>
2018-06-30 14:30:47 +0800

    Initial Commit

---
+++ ImageEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageEditor
+//
+//  Created by mrfour on 2018/6/30.
+//  Copyright © 2018 mrfour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
